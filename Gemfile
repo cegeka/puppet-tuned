8cff4a1aefe73d924bae2d204e1c5b8a561f9966
bovy89 <bovy89@gmail.com>
2017-08-16 14:03:17 +0200

    fix CI setup

--- Gemfile
+++ Gemfile
@@ -1,22 +1,20 @@
 source ENV['GEM_SOURCE'] || "https://rubygems.org"
 
-puppetversion = ENV.key?('PUPPET_GEM_VERSION') ? "= #{ENV['PUPPET_GEM_VERSION']}" : ['>= 3.3']
-gem 'puppet', puppetversion
-gem 'facter', '>= 1.7.0'
+ENV['PUPPET_GEM_VERSION'].nil? ? puppetversion = '~> 3.0' : puppetversion = ENV['PUPPET_GEM_VERSION'].to_s
+gem 'puppet', puppetversion, :require => false
+gem 'facter', "~> 2.4.0",    :require => false
 
 group :development, :unit_tests do
-  gem 'rake',                                                      '< 11.0.0'
   gem 'rspec-puppet',                                              :require => false
   gem 'parallel_tests', "~> 2.9.0",                                :require => false
   gem 'rspec-puppet-facts',                                        :require => false
   gem 'puppetlabs_spec_helper',                                    :require => false
   gem 'puppet-lint', "1.0.1",                                      :require => false
-  gem 'json', "~> 1.8.3",                                          :require => false
-  gem 'json_pure', "~> 1.8.3",                                     :require => false
   gem 'puppet-syntax',                                             :require => false
-  gem 'metadata-json-lint',                                        :require => false
+  gem 'metadata-json-lint', "~> 1.1.0",                            :require => false
   gem 'puppet-blacksmith',                                         :require => false
-  gem 'rest-client', "1.6.8",                                      :require => false
+  gem 'mime-types', "~> 2.99",                                     :require => false if RUBY_VERSION < '2.0.0'
+  gem 'rspec-puppet-utils',                                        :require => false
   gem 'puppet-lint-absolute_classname-check',                      :require => false
   gem 'puppet-lint-appends-check',                                 :require => false
   gem 'puppet-lint-empty_string-check',                            :require => false
@@ -31,18 +29,21 @@ group :development, :unit_tests do
   gem 'puppet-lint-alias-check',                                   :require => false
   gem 'puppet-lint-classes_and_types_beginning_with_digits-check', :require => false
   gem 'puppet-lint-file_source_rights-check',                      :require => false
-
+  gem 'tins', '1.6.0',                                             :require => false
+  gem 'term-ansicolor', "1.3.2",                                   :require => false if RUBY_VERSION < '2.0.0'
+  gem 'coveralls',                                                 :require => false
+  gem 'simplecov-console',                                         :require => false
+  gem 'rubocop', '~> 0.49.1',                                      :require => false if RUBY_VERSION >= '2.3.0'
+  gem 'rubocop-rspec', '~> 1.15.0',                                :require => false if RUBY_VERSION >= '2.3.0'
 end
 
 group :system_tests do
-  gem 'beaker-rspec',    :require => false
-  gem 'serverspec',      :require => false
-  gem 'vagrant-wrapper', :require => false
+  gem 'beaker-rspec',                 :require => false
+  gem 'serverspec',                   :require => false
+  gem 'vagrant-wrapper',              :require => false
+  gem 'beaker-puppet_install_helper', :require => false
 end
 
 group :development do
-  gem 'simplecov',          :require => false
-  gem 'guard-rake',         :require => false
-  gem 'listen', '~> 3.0.0', :require => false
+  gem 'guard-rake', :require => false
 end
-
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,60 +2,83 @@ require 'spec_helper'
 
 
 describe 'tuned', :type => :class do
-  context "on a CentoOS 7 OS" do
-    let(:facts) do
-      {
-        :osfamily                  => 'RedHat',
-        :operatingsystemmajrelease => '7',
-        :operatingsystemrelease    => '7.0.1406',
-        :operatingsystem           => 'CentOS',
-      }
-    end
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
 
-    context 'with default values for all parameters' do
-      it { should create_class('tuned') }
-      it { should compile }
-      it { should contain_file('/etc/tuned/bootcmdline') }
-      it { should contain_file('/etc/tuned/tuned-main.conf') }
-      it { should contain_service('tuned').with(
-        :enable => true,
-        :ensure => 'running',
-        :name   => 'tuned',
-      )}
+      describe 'with default values for all parameters' do
+        it { is_expected.to create_class('tuned') }
+        it { is_expected.to compile }
 
-      it { should contain_package('tuned').with(
-        :ensure => 'present'
-      )}
+        case facts[:operatingsystemmajrelease]
+        when '6'
+          it { is_expected.to contain_file('/etc/tune-profiles/bootcmdline') }
+          it { is_expected.to contain_file('/etc/tune-profiles/tuned-main.conf') }
+          it { is_expected.to contain_service('ktune').with(
+            :enable => true,
+            :ensure => 'running',
+          )}
+        else
+          it { is_expected.to contain_file('/etc/tuned/bootcmdline') }
+          it { is_expected.to contain_file('/etc/tuned/tuned-main.conf') }
+        end
 
-      it { should_not contain_class('tuned::profile::enable_profile') }
-    end
+        it { is_expected.to contain_service('tuned').with(
+          :enable => true,
+          :ensure => 'running',
+        )}
 
-    context 'with service_ensure => stopped' do
-      let(:params) { { :service_ensure => 'stopped' } }
-      it { should contain_service('tuned').with(
-        :enable => true,
-        :ensure => params[:service_ensure],
-        :name   => 'tuned',
-      )}
-    end
+        it { is_expected.to contain_package('tuned').with(
+          :ensure => 'present'
+        )}
 
-    context 'with tuned_ensure => absent' do
-      let(:params) { { :tuned_ensure => 'absent' } }
-      it { should contain_package('tuned').with(
-        :ensure => params[:tuned_ensure],
-      )}
-    end
+        it { is_expected.not_to contain_class('tuned::profile::enable_profile') }
+      end
 
-    context 'with active_profile => virtual-guest' do
-      let(:params) { { :active_profile => 'virtual-guest' } }
-      it { should contain_class('tuned::profile::enable_profile').with(
-        :profile_name => params[:active_profile],
-      )}
+      describe 'with service_ensure => stopped' do
+        let(:params) { { :service_ensure => 'stopped' } }
+        it { is_expected.to contain_service('tuned').with(
+          :enable => true,
+          :ensure => params[:service_ensure],
+        )}
 
-      it { should contain_exec("tuned-adm_enable_profile_#{params[:active_profile]}").with(
-        :command => "tuned-adm profile #{params[:active_profile]}",
-        :unless  => "grep -q -e '^#{params[:active_profile]}\$' /etc/tuned/active_profile",
-      )}
+        case facts[:operatingsystemmajrelease]
+        when '6'
+          it { is_expected.to contain_service('ktune').with(
+            :enable => true,
+            :ensure => params[:service_ensure],
+          )}
+        end
+      end
+
+      describe 'with tuned_ensure => absent' do
+        let(:params) { { :tuned_ensure => 'absent' } }
+        it { is_expected.to contain_package('tuned').with(
+          :ensure => params[:tuned_ensure],
+        )}
+      end
+
+      describe 'with active_profile => virtual-guest' do
+        let(:params) { { :active_profile => 'virtual-guest' } }
+        it { is_expected.to contain_class('tuned::profile::enable_profile').with(
+          :profile_name => params[:active_profile],
+        )}
+
+        case facts[:operatingsystemmajrelease]
+        when '6'
+          it { is_expected.to contain_exec("tuned-adm_enable_profile_#{params[:active_profile]}").with(
+            :command => "tuned-adm profile #{params[:active_profile]}",
+            :unless  => "grep -q -e '^#{params[:active_profile]}\$' /etc/tune-profiles/active_profile",
+          )}
+        else
+          it { is_expected.to contain_exec("tuned-adm_enable_profile_#{params[:active_profile]}").with(
+            :command => "tuned-adm profile #{params[:active_profile]}",
+            :unless  => "grep -q -e '^#{params[:active_profile]}\$' /etc/tuned/active_profile",
+          )}
+        end
+      end
     end
   end
 end
